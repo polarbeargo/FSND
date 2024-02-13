e2c28dc127bea439fea127b0ba459574c12b65be
Hsin-Wen Chang <bow1226@gmail.com>
2024-02-13 18:14:56 -0500

    feat: Implement data models

--- projects/capstone/heroku_sample/models.py
+++ projects/capstone/heroku_sample/models.py
@@ -5,7 +5,7 @@ import json
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
-  database_path = database_path.replace("postgres://", "postgresql://", 1)
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy()
 
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,23 +23,74 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
 '''
-Person
-Have title and release year
+ Movie table & Model
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+class Movie(db.Model):
+    __tablename__ = 'Movies'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    id = Column(db.Integer, primary_key=True)
+    title = Column(String)
+    release_year = Column(String)
+
+    def __init__(self, title, release_year):
+        self.title = title
+        self.release_year = release_year
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_year': self.release_year}
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+  Actor table & Model
+  '''
+    class Actor(db.Model):
+        __tablename__ = 'Actors'
+
+        id = Column(db.Integer, primary_key=True)
+        name = Column(String)
+        age = Column(String)
+        gender = Column(String)
+
+        def __init__(self, name, age, gender):
+            self.name = name
+            self.age = age
+            self.gender = gender
+
+        def format(self):
+            return {
+                'id': self.id,
+                'name': self.name,
+                'age': self.age,
+                'gender': self.gender}
+
+        def insert(self):
+            db.session.add(self)
+            db.session.commit()
+
+        def update(self):
+            db.session.commit()
+
+        def delete(self):
+            db.session.delete(self)
+            db.session.commit()
