0a9ac486718f03cb580b0b9e2eafb61e57f01604
Hsin-Wen Chang <bow1226@gmail.com>
2024-03-09 09:41:12 -0500

    chore: Update requirements.txt & setup.sh

--- projects/capstone/heroku_sample/setup.sh
+++ projects/capstone/heroku_sample/setup.sh
@@ -2,3 +2,7 @@
 export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
+echo $DATABASE_URL
+echo $EXCITED
+export FLASK_APP=myapp
+export FLASK_ENV=development
