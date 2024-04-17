f2d26a1c07e57ca8a6fba0aad883771518f48e3c
Hsin-Wen Chang <bow1226@gmail.com>
2024-04-17 19:42:26 +0800

    refactor: Implement os.getenv to get database_path

--- projects/capstone/heroku_sample/models.py
+++ projects/capstone/heroku_sample/models.py
@@ -3,16 +3,14 @@ from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "capstone"
-database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
+database_path = os.getenv('DATABASE_URL')
+
 db = SQLAlchemy()
 
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-
-
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
